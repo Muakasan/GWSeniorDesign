b836092c7cdd69203a6694c15b5bc270f396592b
muakasan <muakasan@gmail.com>
2015-06-25 12:25:43 -0400

    changed template, working on creating folders script

--- CreateUserFolders.py
+++ CreateUserFolders.py
@@ -1,7 +1,33 @@
-import copyfile from shutil
+from shutil import copyfile 
 
-directory = "2015/"
-directory = "2014/"
-directory = "2013/"
-directory = "2012/"
-directory = "2011/"
+def getStudentNames():
+	l = [[2011],[2012],[2013],[2014],[2015]]
+	infile = open("users.txt", "r")
+	year = 0
+	while(True):
+		lineStng = infile.readline()
+		if lineStng=="":
+			break
+		lineStng = lineStng[:len(lineStng)-1]
+		if lineStng == "":
+			year+=1
+		else:
+			l[year].append(lineStng)
+	print(l)
+	infile.close()
+	return l
+def getTemplate():
+	infile = open("student_bio_template.html")
+def createFolders(directory):
+	print("hello")
+
+def main():
+	l = getStudentNames()
+	for i in l:
+		directory = n[0]+'/'
+		for j in i:
+			nameStng = i.replace(" ", "_")
+			outfile = open(nameStng+".html", "w+")
+			
+
+main()
